b1ecb6c3e3329e8dcd8e7ef39fa33b5637370ed7
adeetiborkar <borkar.adeeti@gmail.com>
2020-05-11 18:19:45 +0530

    new architect added

---
+++ src/testtemp/Date_Time.java
@@ -0,0 +1,27 @@
+package testtemp;
+
+import java.time.*;										//Package for date and time
+import java.time.format.DateTimeFormatter;				//Package for Date formatter
+
+public class Date_Time
+{
+	public static void main(String[] args)
+	{
+		//Postjira 2
+		LocalDate myDate = LocalDate.now();					  // Local date class object
+		System.out.println("Local Date is :  " +myDate);
+		
+		LocalTime myTime = LocalTime.now();						// Local time class object
+		System.out.println("Local Time is :  " + myTime);
+		
+		LocalDateTime myDateTime= LocalDateTime.now();			// Local dateTime class object
+		System.out.println("Local Date & Time is :  " + myDateTime);
+		
+		// Class for Date and Time formatter
+		
+		DateTimeFormatter myFormatter= DateTimeFormatter.ofPattern ("dd/MM/yyyy   HH:mm:ss");
+		String formatted =myDateTime.format(myFormatter);
+		System.out.println("Formatter Date is :  " + formatted);
+	}
+
+}
